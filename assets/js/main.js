7f4ca7b79a33805b789be737b0758f925f2890c5
Srujan Papaiahgari <19625909+srujanpapaiah@users.noreply.github.com>
2022-12-08 18:34:34 +0530

    swiper bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,15 +96,15 @@ modalCloses.forEach((modalClose) => {
 // import "swiper/swiper-bundle.css";
 let swiper = new Swiper(".portfolio__container", {
   cssMode: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  mousewheel: true,
-  keyboard: true,
 });
 
 /*==================== TESTIMONIAL ====================*/
