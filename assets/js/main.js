7ab3f0d1d5373ce2909fe2a31be3117604e11c61
Srujan Papaiahgari <19625909+srujanpapaiah@users.noreply.github.com>
2022-12-08 18:00:35 +0530

    added swiperjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,8 +69,43 @@ tabs.forEach((tab) => {
   });
 });
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+// import Swiper from "swiper/swiper-bundle.esm.js";
+// import "swiper/swiper-bundle.css";
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
 /*==================== TESTIMONIAL ====================*/
 
